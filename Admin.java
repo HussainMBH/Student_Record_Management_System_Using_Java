256f214f593b3e728488f0c53436432f1ebbe207
Bahir Hussain <mbahirh624@gmail.com>
2024-05-18 13:11:25 +0530

    Update

--- Admin.java
+++ Admin.java
@@ -5,35 +5,35 @@ public class Admin{
     
     private static int choiceId;
     
-    // Declaring ANSI_RESET 
-    public static final String ANSI_RESET = "\u001B[0m"; 
+    // Declaring Reset
+    public static final String Reset = "\u001B[0m";
   
     // Declaring CLI colors
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String Green = "\u001B[32m";
+    public static final String Yellow = "\u001B[33m";
+    public static final String Blue = "\u001B[34m";
     public static final String ANSI_RED_BG = "\u001B[41m";
-    public static final String ANSI_GREEN_BG = "\u001B[42m";
-    public static final String ANSI_BLUE_BG = "\u001B[44m";
-    public static final String ANSI_YELLOW_BG = "\u001B[43m";
+    public static final String Green_BG = "\u001B[42m";
+    public static final String Blue_BG = "\u001B[44m";
+    public static final String Yellow_BG = "\u001B[43m";
 
     // Initialize Scanner
     private static Scanner input = new Scanner(System.in);
     
     public static void main(String args[]) {
         do {
-            System.out.print(ANSI_BLUE + "+ ---------------------------------------------------------- +" + ANSI_RESET + "\n\n");
-            System.out.println("\t" + ANSI_BLUE + "===== STUDENT RECORD MANAGEMENT SYSTEM =====" + ANSI_RESET + "\n");
-            System.out.println("\t" + ANSI_BLUE + "=> Add a New Student ********** " + ANSI_RESET + ANSI_BLUE_BG + " 1 " + ANSI_RESET);
-            System.out.println("\t" + ANSI_BLUE + "=> Update a Student Record **** " + ANSI_RESET + ANSI_BLUE_BG + " 2 " + ANSI_RESET);
-            System.out.println("\t" + ANSI_BLUE + "=> View a Student Record ****** " + ANSI_RESET + ANSI_BLUE_BG + " 3 " + ANSI_RESET);
-            System.out.println("\t" + ANSI_BLUE + "=> Delete a Student Record **** " + ANSI_RESET + ANSI_BLUE_BG + " 4 " + ANSI_RESET);
-            System.out.println("\t" + ANSI_BLUE + "=> View All Student Records *** " + ANSI_RESET + ANSI_BLUE_BG + " 5 " + ANSI_RESET);
-            System.out.println("\t" + ANSI_BLUE + "=> Get Rankings *************** " + ANSI_RESET + ANSI_BLUE_BG + " 6 " + ANSI_RESET);
-            System.out.println("\t" + ANSI_BLUE + "=> Exit *********************** " + ANSI_RESET + ANSI_BLUE_BG + " 7 " + ANSI_RESET);
-            System.out.println("\n" + ANSI_BLUE + "+ ---------------------------------------------------------- +" + ANSI_RESET + "\n");
+            System.out.print(Blue + "+ ---------------------------------------------------------- +" + Reset + "\n\n");
+            System.out.println("\t" + Blue + "===== STUDENT RECORD MANAGEMENT SYSTEM =====" + Reset + "\n");
+            System.out.println("\t" + Blue + "=> Add a New Student ********** " + Reset + Blue_BG + " 1 " + Reset);
+            System.out.println("\t" + Blue + "=> Update a Student Record **** " + Reset + Blue_BG + " 2 " + Reset);
+            System.out.println("\t" + Blue + "=> View a Student Record ****** " + Reset + Blue_BG + " 3 " + Reset);
+            System.out.println("\t" + Blue + "=> Delete a Student Record **** " + Reset + Blue_BG + " 4 " + Reset);
+            System.out.println("\t" + Blue + "=> View All Student Records *** " + Reset + Blue_BG + " 5 " + Reset);
+            System.out.println("\t" + Blue + "=> Get Rankings *************** " + Reset + Blue_BG + " 6 " + Reset);
+            System.out.println("\t" + Blue + "=> Exit *********************** " + Reset + Blue_BG + " 7 " + Reset);
+            System.out.println("\n" + Blue + "+ ---------------------------------------------------------- +" + Reset + "\n");
             
-            System.out.print(ANSI_BLUE_BG + "Enter Your Choice:" + ANSI_RESET);
+            System.out.print(Blue_BG + "Enter Your Choice:" + Reset);
             System.out.print(" ");
             choiceId = input.nextInt();
 
@@ -75,17 +75,17 @@ public class Admin{
     // add a new student to the system
     public static void createAStudent() {
         try {
-            System.out.print(ANSI_YELLOW + "Enter the student name: " + ANSI_RESET);
+            System.out.print(Yellow + "Enter the student name: " + Reset);
             String name = input.nextLine();
             
-            System.out.print(ANSI_YELLOW + "Enter the Mathematics mark: " + ANSI_RESET);
+            System.out.print(Yellow + "Enter the Mathematics mark: " + Reset);
             int maths = input.nextInt();
             
             if(maths < 0 || maths > 100) {
                 throw new Exception("Invalid Marks, Marks should be between 0 and 100!");
             }
             
-            System.out.print(ANSI_YELLOW + "Enter the Science mark: " + ANSI_RESET);
+            System.out.print(Yellow + "Enter the Science mark: " + Reset);
             int science = input.nextInt();
             
             if(science < 0 || science > 100) {
@@ -96,14 +96,14 @@ public class Admin{
             String[][] marks = {{"Mathematics", Integer.toString(maths)}, {"Science", Integer.toString(science)}};
         
             if(StudentManage.setStudentsList(name, marks)) {
-                System.out.println("\n" + ANSI_GREEN_BG + "Successfully Added a New Student!" + ANSI_RESET);
+                System.out.println("\n" + Green_BG + "Successfully Added a New Student!" + Reset);
                 System.out.println();
             } else {
                 throw new Exception("Can't Add a New Student!");
             }
         } catch (Exception e) {
             System.out.println();
-            System.out.println(ANSI_RED_BG + e + ANSI_RESET);
+            System.out.println(ANSI_RED_BG + e + Reset);
             System.out.println();
             System.exit(0);
         }
@@ -111,9 +111,9 @@ public class Admin{
     
     // selector for update student mark or name
     public static void updateChoice() {
-        System.out.println(ANSI_YELLOW + "=> Student name update ***** " + ANSI_RESET + ANSI_YELLOW_BG + " 1 " + ANSI_RESET);
-        System.out.println(ANSI_YELLOW + "=> Student mark update ***** " + ANSI_RESET + ANSI_YELLOW_BG + " 2 " + ANSI_RESET);
-        System.out.print("\n" + ANSI_BLUE_BG + "Enter your choice:" + ANSI_RESET + " ");
+        System.out.println(Yellow + "=> Student name update ***** " + Reset + Yellow_BG + " 1 " + Reset);
+        System.out.println(Yellow + "=> Student mark update ***** " + Reset + Yellow_BG + " 2 " + Reset);
+        System.out.print("\n" + Blue_BG + "Enter your choice:" + Reset + " ");
 
         String userChoice = input.nextLine();
         System.out.println();
@@ -129,20 +129,20 @@ public class Admin{
     
     // student name update
     public static void updateStudentName() {
-        System.out.print(ANSI_YELLOW + "Enter the student ID:" + ANSI_RESET + " ");
+        System.out.print(Yellow + "Enter the student ID:" + Reset + " ");
         String id = input.nextLine();
 
-        System.out.print(ANSI_YELLOW + "Enter the student name:" + ANSI_RESET + " ");
+        System.out.print(Yellow + "Enter the student name:" + Reset + " ");
         String name = input.nextLine();
 
         boolean message = StudentManage.setStudentName(id, name);
         
         if(message) {
-            System.out.println("\n" + ANSI_GREEN_BG + "Successfully Name Updated!" + ANSI_RESET);
+            System.out.println("\n" + Green_BG + "Successfully Name Updated!" + Reset);
             System.out.println();
         } else {
             System.out.println();
-            System.out.println(ANSI_RED_BG + "Error! Can't Update Name. Check Student ID Again." + ANSI_RESET);
+            System.out.println(ANSI_RED_BG + "Error! Can't Update Name. Check Student ID Again." + Reset);
             System.out.println();
         }
     }
@@ -152,22 +152,22 @@ public class Admin{
         String[] availableSubjects = {"Mathematics", "Science"};
         boolean message = false;
 
-        System.out.print(ANSI_YELLOW + "Enter the student ID: " + ANSI_RESET);
+        System.out.print(Yellow + "Enter the student ID: " + Reset);
         String id = input.nextLine();
 
         // display all available subjects
-        System.out.println("\n" + ANSI_GREEN_BG + "=== Available Subjects ===" + ANSI_RESET + "\n");
+        System.out.println("\n" + Green_BG + "=== Available Subjects ===" + Reset + "\n");
         
         for(String subject : availableSubjects) {
-            System.out.println(ANSI_GREEN + "=> " + subject + ANSI_RESET);
+            System.out.println(Green + "=> " + subject + Reset);
         }
 
         System.out.println();
         
-        System.out.print(ANSI_YELLOW + "Enter the subject name: " + ANSI_RESET);
+        System.out.print(Yellow + "Enter the subject name: " + Reset);
         String subject = input.nextLine();
         
-        System.out.print(ANSI_YELLOW + "Enter the subject mark: " + ANSI_RESET);
+        System.out.print(Yellow + "Enter the subject mark: " + Reset);
         int mark = input.nextInt();
 
         if(mark >= 0 && mark <= 100) {
@@ -175,33 +175,33 @@ public class Admin{
         }
 
         if(message) {
-            System.out.println("\n" + ANSI_GREEN_BG + "Successfully Mark Updated!" + ANSI_RESET);
+            System.out.println("\n" + Green_BG + "Successfully Mark Updated!" + Reset);
             System.out.println();
         } else {
             System.out.println();
-            System.out.println(ANSI_RED_BG + "Error! Can't Update Mark" + ANSI_RESET);
+            System.out.println(ANSI_RED_BG + "Error! Can't Update Mark" + Reset);
             System.out.println();
         }
     }
     
     public static void removeStudent() {
-        System.out.print(ANSI_YELLOW + "Enter the student ID: " + ANSI_RESET);
+        System.out.print(Yellow + "Enter the student ID: " + Reset);
         String id = input.nextLine();
 
         boolean success = StudentManage.deleteAStudent(id);
         
         if(success) {
-            System.out.println("\n" + ANSI_GREEN_BG + "Successfully Deleted!" + ANSI_RESET);
+            System.out.println("\n" + Green_BG + "Successfully Deleted!" + Reset);
             System.out.println();
         } else {
             System.out.println();
-            System.out.println(ANSI_RED_BG + "Error! Can't Delete. Check Student ID." + ANSI_RESET);
+            System.out.println(ANSI_RED_BG + "Error! Can't Delete. Check Student ID." + Reset);
             System.out.println();
         }
     }
     
     public static void displayAStudent() {
-        System.out.print(ANSI_YELLOW + "Enter the student ID: " + ANSI_RESET);
+        System.out.print(Yellow + "Enter the student ID: " + Reset);
         String id = input.nextLine();
         
         StudentManage.getAStudentData(id);
